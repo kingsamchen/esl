ca65e722a4101e244945923d65423f60b172c311
Kingsley Chen <kingsamchen@gmail.com>
2021-11-07 23:49:01 +0800

    Add unique_ptr-like wrapper for POSIX fd and Windows HANDLE

---
+++ esl/unique_handle.h
@@ -0,0 +1,138 @@
+// Copyright (c) 2021 Kingsley Chen <kingsamchen at gmail dot com>
+// This file is subject to the terms of license that can be found
+// in the LICENSE file.
+
+#pragma once
+
+#ifndef ESL_UNIQUE_HANDLE_H_
+#define ESL_UNIQUE_HANDLE_H_
+
+#include <memory>
+
+#if defined(_WIN32)
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
+namespace esl {
+
+template<typename Traits>
+class handle_ptr {
+public:
+    using handle_type = typename Traits::handle_type;
+
+    handle_ptr() noexcept = default;
+
+    // implicit
+    handle_ptr(std::nullptr_t) noexcept {}
+
+    // implicit
+    handle_ptr(handle_type handle) noexcept
+        : handle_(handle) {}
+
+    ~handle_ptr() = default;
+
+    explicit operator bool() const noexcept {
+        return Traits::is_valid(handle_);
+    }
+
+    // implicit
+    operator handle_type() const noexcept {
+        return handle_;
+    }
+
+    friend bool operator==(handle_ptr lhs, handle_ptr rhs) noexcept {
+        return lhs.handle_ == rhs.handle_;
+    }
+
+    friend bool operator!=(handle_ptr lhs, handle_ptr rhs) noexcept {
+        return !(lhs == rhs);
+    }
+
+private:
+    handle_type handle_{Traits::null_handle};
+};
+
+template<typename Traits>
+struct handle_ptr_deleter {
+    using pointer = handle_ptr<Traits>;
+
+    void operator()(pointer ptr) {
+        Traits::close(ptr);
+    }
+};
+
+#if defined(_WIN32)
+
+struct win_handle_traits {
+    using handle_type = HANDLE;
+
+    static bool is_valid(handle_type handle) noexcept {
+        return handle != null_handle;
+    }
+
+    static void close(handle_type handle) noexcept {
+        ::CloseHandle(handle);
+    }
+
+    static constexpr handle_type null_handle{nullptr};
+};
+
+using win_handle_deleter = handle_ptr_deleter<win_handle_traits>;
+using unique_win_handle = std::unique_ptr<win_handle_traits::handle_type, win_handle_deleter>;
+
+inline unique_win_handle wrap_unique_win_handle(HANDLE raw_handle) {
+    return unique_win_handle(raw_handle);
+}
+
+struct winfile_handle_traits {
+    using handle_type = HANDLE;
+
+    static bool is_valid(handle_type handle) noexcept {
+        return handle != null_handle;
+    }
+
+    static void close(handle_type handle) noexcept {
+        ::CloseHandle(handle);
+    }
+
+    static constexpr handle_type null_handle{INVALID_HANDLE_VALUE};
+};
+
+using winfile_handle_deleter = handle_ptr_deleter<winfile_handle_traits>;
+using unique_winfile_handle =
+    std::unique_ptr<winfile_handle_traits::handle_type, winfile_handle_deleter>;
+
+inline unique_winfile_handle wrap_unique_winfile_handle(HANDLE raw_file_handle) {
+    return unique_winfile_handle(raw_file_handle);
+}
+
+#else
+
+struct fd_traits {
+    using handle_type = int;
+
+    static bool is_valid(handle_type handle) noexcept {
+        return handle != null_handle;
+    }
+
+    static void close(handle_type handle) noexcept {
+        ::close(handle);
+    }
+
+    static constexpr handle_type null_handle{-1};
+};
+
+using fd_deleter = handle_ptr_deleter<fd_traits>;
+using unique_fd = std::unique_ptr<fd_traits::handle_type, fd_deleter>;
+
+inline unique_fd wrap_unique_fd(int raw_fd) {
+    return unique_fd(raw_fd);
+}
+
+#endif
+
+} // namespace esl
+
+#endif // ESL_UNIQUE_HANDLE_H_
---
+++ tests/unique_handle_test.cpp
@@ -0,0 +1,214 @@
+// Copyright (c) 2021 Kingsley Chen <kingsamchen at gmail dot com>
+// This file is subject to the terms of license that can be found
+// in the LICENSE file.
+
+#include <type_traits>
+
+#include "doctest/doctest.h"
+
+#include "esl/unique_handle.h"
+
+#if !defined(_WIN32)
+#include <fcntl.h>
+#endif
+
+namespace {
+
+struct fake_handle_traits {
+    using handle_type = int;
+
+    static bool is_valid(handle_type handle) noexcept {
+        return handle != null_handle;
+    }
+
+    static void close(handle_type) noexcept {
+        // do nothing
+    }
+
+    static constexpr handle_type null_handle{-1};
+};
+
+template<typename T, typename = void>
+struct is_equality_comparable : std::false_type {};
+
+template<typename T>
+struct is_equality_comparable<
+    T,
+    std::void_t<
+        std::enable_if_t<
+            std::conjunction_v<
+                std::is_same<bool, decltype(T{} == T{})>,
+                std::is_same<bool, decltype(T{} != T{})>>>>> : std::true_type {};
+
+template<typename T>
+inline constexpr bool is_equality_comparable_v = is_equality_comparable<T>::value;
+
+TEST_SUITE_BEGIN("unique_handle");
+
+TEST_CASE("handle_ptr must meet nullability expression requirements") {
+    using fake_handle_ptr = esl::handle_ptr<fake_handle_traits>;
+
+    SUBCASE("satisfy type traits") {
+        CHECK(is_equality_comparable_v<fake_handle_ptr>);
+        CHECK(std::is_default_constructible_v<fake_handle_ptr>);
+        CHECK(std::is_copy_constructible_v<fake_handle_ptr>);
+        CHECK(std::is_copy_assignable_v<fake_handle_ptr>);
+        CHECK(std::is_destructible_v<fake_handle_ptr>);
+    }
+
+    SUBCASE("value initialized handle_ptr must produce a null value of that type") {
+        fake_handle_ptr ptr{};
+        // Relies on implicit conversion.
+        CHECK(ptr == fake_handle_traits::null_handle);
+    }
+
+    SUBCASE("must be contextually convertible to bool") {
+        {
+            fake_handle_ptr ptr1{};
+            bool valid = static_cast<bool>(ptr1);
+            CHECK_FALSE(valid);
+        }
+
+        {
+            fake_handle_ptr ptr2{1};
+            bool valid = static_cast<bool>(ptr2);
+            CHECK(valid);
+        }
+    }
+
+    SUBCASE("must satisfy following additional expressions") {
+        SUBCASE("construction from nullptr") {
+            fake_handle_ptr ptr1(nullptr);
+            CHECK(ptr1 == nullptr);
+
+            fake_handle_ptr ptr2 = nullptr;
+            CHECK(ptr2 == nullptr);
+        }
+
+        SUBCASE("a temporary from nullptr is equivalent to nullptr") {
+            CHECK(fake_handle_ptr(nullptr) == nullptr);
+        }
+
+        SUBCASE("compare with nullptr") {
+            fake_handle_ptr np_ptr(nullptr);
+            CHECK(np_ptr == nullptr);
+            CHECK(nullptr == np_ptr);
+            CHECK_FALSE(np_ptr != nullptr);
+            CHECK_FALSE(nullptr != np_ptr);
+
+            fake_handle_ptr ptr(1);
+            CHECK_FALSE(ptr == nullptr);
+            CHECK_FALSE(nullptr == ptr);
+            CHECK(ptr != nullptr);
+            CHECK(nullptr != ptr);
+        }
+
+        SUBCASE("compare with other handle_ptr") {
+            CHECK(fake_handle_ptr(nullptr) == fake_handle_ptr(nullptr));
+            CHECK(fake_handle_ptr(nullptr) != fake_handle_ptr(1));
+        }
+
+        SUBCASE("equivalent to nullptr after assigned to nullptr") {
+            fake_handle_ptr ptr(1);
+            CHECK(ptr != nullptr);
+            ptr = nullptr;
+            CHECK(ptr == nullptr);
+        }
+    }
+
+    SUBCASE("value initialized handle_ptr is equivalent to nullptr") {
+        CHECK(fake_handle_ptr{} == nullptr);
+        CHECK_FALSE(fake_handle_ptr{} != nullptr);
+    }
+}
+
+#if defined(_WIN32)
+
+TEST_CASE("unique_win_handle should work as expected") {
+    using esl::unique_win_handle;
+    using esl::wrap_unique_win_handle;
+
+    SUBCASE("default initialized should be equivalent to nullptr") {
+        unique_win_handle handle;
+        CHECK(handle == nullptr);
+    }
+
+    SUBCASE("INVALID_HANDLE_VALUE is a valid value for unique_win_handle") {
+        auto handle = wrap_unique_win_handle(INVALID_HANDLE_VALUE);
+        CHECK(handle != nullptr);
+    }
+
+    SUBCASE("non-empty handle") {
+        static constexpr wchar_t event_name[] = L"test_event";
+
+        auto handle = wrap_unique_win_handle(::CreateEventW(nullptr, TRUE, TRUE, event_name));
+        REQUIRE(handle != nullptr);
+        REQUIRE(handle.get() != nullptr);
+        auto sync_handle = wrap_unique_win_handle(::OpenEventW(SYNCHRONIZE, FALSE, event_name));
+        REQUIRE(sync_handle != nullptr);
+        sync_handle.reset();
+        REQUIRE(sync_handle == nullptr);
+
+        handle.reset();
+        CHECK(handle == nullptr);
+        CHECK_EQ(::OpenEventW(SYNCHRONIZE, FALSE, event_name), nullptr);
+    }
+}
+
+TEST_CASE("unique_winfile_handle should satisfy") {
+    using esl::unique_winfile_handle;
+    using esl::wrap_unique_winfile_handle;
+
+    SUBCASE("default initialized should be") {
+        unique_winfile_handle handle;
+
+        SUBCASE("equivalent to nullptr") {
+            CHECK(handle == nullptr);
+        }
+
+        SUBCASE("handle value is equal to INVALID_HANDLE_VALUE") {
+            CHECK(handle.get() == INVALID_HANDLE_VALUE);
+            CHECK(handle.get() != static_cast<HANDLE>(0)); // NOLINT
+        }
+    }
+
+    SUBCASE("0 handle value is a valid value for unique_winfile_handle") {
+        auto handle = wrap_unique_winfile_handle(static_cast<HANDLE>(0));
+        CHECK(handle != nullptr);
+    }
+}
+
+#else
+
+TEST_CASE("unique_fd should satisfy") {
+    using esl::unique_fd;
+    using esl::wrap_unique_fd;
+
+    SUBCASE("default initialized should be") {
+        unique_fd fd_handle;
+
+        SUBCASE("equivalent to nullptr") {
+            CHECK(fd_handle == nullptr);
+        }
+
+        SUBCASE("fd value is -1") {
+            CHECK(fd_handle.get() == -1);
+        }
+    }
+
+    SUBCASE("simple use cases") {
+        auto fd = wrap_unique_fd(open("/proc/cpuinfo", O_RDONLY));
+        REQUIRE(fd != nullptr);
+        REQUIRE(fd.get() != -1);
+
+        fd.reset();
+        CHECK(fd == nullptr);
+        CHECK(fd.get() == -1);
+    }
+}
+
+#endif
+
+TEST_SUITE_END();
+
+} // namespace
