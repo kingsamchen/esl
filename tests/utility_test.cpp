4b7529a1585a5a6d2940a15c98d3df79bb34f2d1
Kingsley Chen <kingsamchen@gmail.com>
2025-11-22 17:07:01 +0800

    Add struct overloaded

--- tests/utility_test.cpp
+++ tests/utility_test.cpp
@@ -3,6 +3,8 @@
 // in the LICENSE file.
 
 #include <cstdint>
+#include <string>
+#include <variant>
 
 #include "doctest/doctest.h"
 
@@ -23,6 +25,30 @@ TEST_CASE("use to_underlying to convert an enum member to its underlying value")
     auto val = esl::to_underlying(flags);
     static_assert(std::is_same_v<decltype(val), std::uint8_t>);
     CHECK_EQ(val, (0x1 << 1));
+}
+
+TEST_CASE("use overloaded for variant visit") {
+    std::variant<int, std::string> vars;
+
+    auto ov = esl::overloaded{
+        [](int) -> int {
+            return 0;
+        },
+        [](const std::string&) -> int {
+            return 1;
+        }};
+
+    SUBCASE("value is int") {
+        vars = 42;
+        const auto idx = std::visit(ov, vars);
+        CHECK_EQ(idx, 0);
+    }
+
+    SUBCASE("value is std::string") {
+        vars = "hello";
+        const auto idx = std::visit(ov, vars);
+        CHECK_EQ(idx, 1);
+    }
 }
 
 TEST_SUITE_END();
